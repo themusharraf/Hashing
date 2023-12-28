bade9d3dc3db362948403836705140ed5460e03c
allnc <meibrohimov@gmail.com>
2023-12-29 02:23:40 +0500

    add hashing write txt file hashing🔒

--- lesson5.py
+++ lesson5.py
@@ -1,11 +1,19 @@
 import hashlib
 
+data: dict = {
+
+    "username": "Anonymous",
+    "password": "admin@33#"
+}
 
 
+def load_hesh(data):  # noqa
+    for key in data.values():
+        sha256 = hashlib.sha256(str(key).encode())
 
-def hashing(data: str):
-    sha256 = hashlib.sha256(data.encode('utf-8'))
-    return sha256.hexdigest()
+        with open("data.txt", "w") as file:
+            txt = file.write(sha256.hexdigest())
+            return txt
 
 
-print(hashing("@Aligator"))
+print(load_hesh(data))
