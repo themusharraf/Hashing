e236091f3f843dd103f1d13275bd2dc22704a5b3
allnc <meibrohimov@gmail.com>
2023-12-29 00:41:51 +0500

    add hashing available

--- lesson.py
+++ lesson.py
@@ -1,9 +1,9 @@
 import hashlib
 
-# hashing functions
-# print(hashlib.algorithms_available)
-
 data = "PDP SCHOOL"
+
 hash_obj = hashlib.md5(str(data).encode('utf-8'))
 
 print(hash_obj.hexdigest())
+
+# c1e4a430ee63f8947535da5c0279a86f
