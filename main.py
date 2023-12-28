8068ff9407859ab5db40f0479785eba0cd3825aa
allnc <meibrohimov@gmail.com>
2023-12-29 00:29:27 +0500

    add hashing available

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import hashlib
 
+# hashing functions
 print(hashlib.algorithms_available)
+# output __dict__
+""" 
+{'shake_128', 'md5', 'sha1', 'sha512', 'sha3_384',
+'sha256', 'sha224', 'sha3_224', 'sha3_512', 'shake_256',
+'sha3_256', 'blake2s', 'blake2b', 'sha384'}
+"""
